f66433fbaf534263dc1669987ae1bbfa0cecb591
François Pluchino <francois.pluchino@sonatra.com>
2015-08-17 13:28:41 +0200

    Format code

--- js/scroller.js
+++ js/scroller.js
@@ -104,9 +104,11 @@
             position,
             maxPosition;
 
-        delta = (event.originalEvent.type === 'DOMMouseScroll' ? event.originalEvent.detail * -40 : event.originalEvent.wheelDelta);
+        delta = (event.originalEvent.type === 'DOMMouseScroll' ? event.originalEvent.detail * -40
+            : event.originalEvent.wheelDelta);
         position = self.getScrollPosition();
-        maxPosition = self.isVertical ? self.$content.get(0).scrollHeight - self.$content.innerHeight() : self.$content.get(0).scrollWidth - self.$content.innerWidth();
+        maxPosition = self.isVertical ? self.$content.get(0).scrollHeight - self.$content.innerHeight()
+            : self.$content.get(0).scrollWidth - self.$content.innerWidth();
 
         if (self.isVertical || (!self.isVertical && event.shiftKey)) {
             if ((delta > 0 && position <= 0) || (delta < 0 && position >= maxPosition)) {
@@ -137,7 +139,8 @@
      * @param {Number}  delta    The delta of translate
      */
     function changeTranslate($target, vertical, delta) {
-        var trans = vertical ? '0px, ' + delta + 'px, 0px' : delta + 'px, 0px, 0px';
+        var trans = vertical ? '0px, ' + delta + 'px, 0px'
+            : delta + 'px, 0px, 0px';
 
         changeTransform($target, 'translate3d(' + trans + ')');
     }
@@ -339,7 +342,8 @@
      * @return {Number}
      */
     Scroller.prototype.setScrollPosition = function (position) {
-        return this.isVertical ? this.$content.scrollTop(position) : this.$content.scrollLeft(position);
+        return this.isVertical ? this.$content.scrollTop(position)
+            : this.$content.scrollLeft(position);
     };
 
     /**
@@ -348,7 +352,8 @@
      * @returns {Number}
      */
     Scroller.prototype.getScrollPosition = function () {
-        return this.isVertical ? this.$content.scrollTop() : this.$content.scrollLeft();
+        return this.isVertical ? this.$content.scrollTop()
+            : this.$content.scrollLeft();
     };
 
     /**
@@ -370,8 +375,10 @@
             return;
         }
 
-        wrapperSize = self.isVertical ? self.$element.innerHeight() : self.$element.innerWidth();
-        contentSize = self.isVertical ? self.$content.get(0).scrollHeight : self.$content.get(0).scrollWidth;
+        wrapperSize = self.isVertical ? self.$element.innerHeight()
+            : self.$element.innerWidth();
+        contentSize = self.isVertical ? self.$content.get(0).scrollHeight
+            : self.$content.get(0).scrollWidth;
         size = Math.max(self.options.scrollbarMinSize, Math.round(wrapperSize * Math.min(wrapperSize / contentSize, 1)));
 
         if (size < wrapperSize) {
@@ -412,10 +419,13 @@
         }
 
         position = this.getScrollPosition();
-        wrapperSize = self.isVertical ? self.$element.innerHeight() : self.$element.innerWidth();
-        contentSize = self.isVertical ? self.$content.get(0).scrollHeight : self.$content.get(0).scrollWidth;
+        wrapperSize = self.isVertical ? self.$element.innerHeight()
+            : self.$element.innerWidth();
+        contentSize = self.isVertical ? self.$content.get(0).scrollHeight
+            : self.$content.get(0).scrollWidth;
         percentScroll = position / (contentSize - wrapperSize);
-        scrollbarSize = self.isVertical ? self.$scrollbar.outerHeight() : self.$scrollbar.outerWidth();
+        scrollbarSize = self.isVertical ? self.$scrollbar.outerHeight()
+            : self.$scrollbar.outerWidth();
         delta = Math.round(percentScroll * (wrapperSize - scrollbarSize));
 
         changeTranslate(self.$scrollbar, self.isVertical, delta);
